9f11d5624717bdfedaa7162e36fd2fbc64d00881
Christopher Nuland <cnuland@localhost.localdomain>
2020-07-24 00:48:58 -0400

    switch to subprocess

--- job/group-label.py
+++ job/group-label.py
@@ -1,20 +1,27 @@
-import commands
+import subprocess
 import os
 import json
 
+def bash_command_pipe(cmd): 
+    ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    return ps.communicate()[0]
+
+def bash_command(cmd): 
+    return subprocess.check_output(cmd)
+
 token = os.getenv('TOKEN')
-commands.getstatusoutput('oc login https://kubernetes.default.svc.cluster.local --token={}'.format(token))
-data = commands.getstatusoutput('oc get cm -n namespace-configuration-operator group-labels -o json | jq -r ".data[]"')
+bash_command('oc login https://kubernetes.default.svc.cluster.local --token={}'.format(token))
+data = bash_command_pipe('oc get cm -n namespace-configuration-operator group-labels -o json | jq -r ".data[]"')
 groups = json.loads(data[1])
 for group in groups:
   name = group["name"]
-  oc=commands.getstatusoutput('oc get group {} -o name --ignore-not-found=true | cut -c 11-'.format(name))
+  oc=bash_command_pipe('oc get group {} -o name --ignore-not-found=true | cut -c 11-'.format(name))
   if oc[1]:
     labels = group["labels"]
     for label in labels:
       key = label["key"]
       value = label["value"]
       print('oc get group {} --template "{{{{ .metadata.labels.{} }}}}"'.format(name, key))
-      oc=commands.getstatusoutput('oc get group {} --template "{{{{ .metadata.labels.{} }}}}"'.format(name, key))
+      oc=bash_command('oc get group {} --template "{{{{ .metadata.labels.{} }}}}"'.format(name, key))
       if "<no value>" in oc[1]:
-        commands.getstatusoutput('oc label group {} {}={}'.format(name, key, value))
+        bash_command('oc label group {} {}={}'.format(name, key, value))
