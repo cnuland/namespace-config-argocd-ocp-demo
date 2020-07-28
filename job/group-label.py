100013a13ad4eb2d03669e9555a7634c98e8a399
Nuland, Chris <Chris.Nuland@anthem.com>
2020-07-28 15:13:24 -0400

    changed group-label

--- job/group-label.py
+++ job/group-label.py
@@ -10,11 +10,14 @@ def bash_command(cmd):
   return subprocess.check_output(cmd)
 
 token = os.getenv('TOKEN')
-bash_command_pipe('oc login https://kubernetes.default.svc.cluster.local --token={} --insecure-skip-tls-verify=true > /dev/null 2>&1'.format(token))
+login = bash_command_pipe('oc login https://kubernetes.default.svc.cluster.local --token={} --insecure-skip-tls-verify=true > /dev/null 2>&1'.format(token))
+print(login)
 data = bash_command_pipe("oc get cm -n namespace-configuration-operator group-labels -o json | jq -r \".data[]\"")
 groups = json.loads(data)
+print("Checking labels on Groups:")
 for group in groups:
   name = group["name"]
+  print(name)
   oc=bash_command_pipe('oc get group {} -o name --ignore-not-found=true | cut -c 11-'.format(name))
   if oc:
     labels = group["labels"]
@@ -23,4 +26,5 @@ for group in groups:
       value = label["value"]
       oc = bash_command_pipe('oc get group {} --template "{{{{ .metadata.labels.{} }}}}"'.format(name, key))
       if "<no value>" in str(oc):
+        print("Adding labels to group {}".format(name))
         bash_command_pipe('oc label group {} {}={}'.format(name, key, value))
